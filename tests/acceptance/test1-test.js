e55544c0c9e315e058c381dfe8a313e7dba2ee5c
collinadams <collinadams04@gmail.com>
2017-10-15 17:15:23 -0700

    Add async and await with ember-concurrency package

--- tests/acceptance/test1-test.js
+++ tests/acceptance/test1-test.js
@@ -3,25 +3,17 @@ import moduleForAcceptance from 'emberli/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | test1');
 
-test('my acceptance test', function(assert) {
-  visit('/');
+test('my acceptance test', async function(assert) {
+  await visit('/');
+  assert.equal(currentURL(), '/', 'foo');
+  assert.ok(find('.course-list-item').length > 2, 'Some courses on the screen'); 
 
-  andThen(function() {
-    assert.equal(currentURL(), '/', 'foo');
-    assert.ok(find('.course-list-item').length > 2, 'Some courses on the screen'); 
-  });
+  await click('.course-list-item[data-name="Ember-CLI Basics"] a');
 
-  click('.course-list-item[data-name="Ember-CLI Basics"] a');
+  assert.equal(currentRouteName(), 'course', 'On the course page');
+  assert.ok(find('li.stage').length > 2, 'Some stages on the screen'); 
 
-  andThen(function(){
-    assert.equal(currentRouteName(), 'course', 'On the course page');
-    assert.ok(find('li.stage').length > 2, 'Some stages on the screen'); 
-  });
-
-  click('ul li:first-child a');
-
-  andThen(function(){
-    assert.equal(currentRouteName(), 'course-stage', 'On the stage page');
-  });
+  await click('ul li:first-child a');
+  assert.equal(currentRouteName(), 'course-stage', 'On the stage page');
 
 });
